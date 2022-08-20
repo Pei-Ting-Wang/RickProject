9ff1aab73171fa345cbf4a8f2097f3c5720a687f
penny <peiting0210@gmail.com>
2022-08-20 15:25:26 +0800

    20220820

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-
 app.use(express.static('public'));
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/question.html'));
-  //__dirname : It will resolve to your project folder.
-});
-router.get('/data',function(req,res){
-    
-    res.send({user:'tobi'});
-    //__dirname : It will resolve to your project folder.
-});
+app.use(express.urlencoded());
+app.use(express.json());
 
+let score;
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/home.html'));
+});
+router.get('/question', function (req, res) {
+    res.sendFile(path.join(__dirname + '/question.html'));
+});
+router.post('/testresult', function (req, res) {
+    score = req.body;
+});
+router.get('/testresult', function (req, res) {
+    res.send(score);
+});
+router.get('/radar', function (req, res) {
+    res.sendFile(path.join(__dirname + '/radar.html'));
+});
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
